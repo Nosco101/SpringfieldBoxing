cb134311c886d2ad971f44bf0c2b16bb116be25f
RyanH1991 <61288676+RyanH1991@users.noreply.github.com>
2022-08-14 13:49:49 -0400

    added golden gloves tab. added poster. started work on profiles.

--- script.js
+++ script.js
@@ -37,3 +37,19 @@ document.getElementById("donate-button-container").addEventListener("mouseover",
 document.getElementById("donate-button-container").addEventListener("mouseout",function() {
   document.getElementById("overbox").style.backgroundColor="#E5C11A";
 });
+
+// collapsible functionality
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
